0318accd6351be2b99af24ceb912e274fb798d1f
naveed <naveedabbasbngsh@gmail.com>
2020-03-28 13:59:33 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+//Main Application Widget
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
+
